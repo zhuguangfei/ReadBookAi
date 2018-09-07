d5947b1c9da91fc93b2ee62071d8516f3c6af9c9
zhuguangfei <1767251400@qq.com>
2018-09-07 09:33:57 +0800

    []

--- baidu_utils.py
+++ baidu_utils.py
@@ -14,8 +14,8 @@ import os
 # print(content)
 # if content:
 # print(content)
-image_file_path = 'source_two/image8'
-text_file_path = 'source_two/text8'
+image_file_path = 'source_two/image9'
+text_file_path = 'source_two/text9'
 if not os.path.exists(text_file_path):
     os.makedirs(text_file_path)
 images = os.listdir(image_file_path)
